74f65c39b07d04d44939619712037776d05781e9
juanrojas53-cmd <juan.rojas53@uptc.edu.co>
2025-08-26 20:29:07 +0000

    operaciones

---
+++ suma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int n1, n2, suma;
+
+        cout<<"ingrese el primer numero"; cin>> n1;
+            cout<<"ingrese el segundo numero"; cin>> n2;
+
+                suma = n1 +n2;
+                    cout << "la suma es:" << suma <<endl;
+
+
+                        return 0;
+                        }
