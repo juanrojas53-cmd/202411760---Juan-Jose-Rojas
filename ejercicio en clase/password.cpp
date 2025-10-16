d5cded4361bfd45501a5c05ad611e1e92a986c39
juanrojas53-cmd <juan.rojas53@uptc.edu.co>
2025-10-16 16:54:18 +0000

    ejercicios

---
+++ ejercicio en clase/password.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+  
+
+    int mayusculas = 0;
+    int minusculas = 0;
+    int spetials = 0;
+     
+bool passwordLenght(string password)
+    {
+        for (int i = 0; i < password.size(); i++)
+        {
+            if(password.size()>=8)
+            return true;
+        }
+        return false;
+
+    }
+
+bool mayusculaLetters(string password)
+    {
+        for (int i = 0; i < password.size(); i++)
+        {
+            if(password[i]>= 65 && password[i]<= 90)
+            return true;
+        }
+        return false;
+    }
+    
+bool minuscula(string password)
+    {
+        for (int i = 0; i < password.size(); i++)
+        {
+            if(password[i]>= 97 && password[i]<= 122)
+            return true;
+        }
+        return false;
+    }
+
+bool espetialCharacter(string password)
+    {
+        for (int i = 0; i < password.size(); i++)
+        {
+            if(password[i]>= 33 && password[i]<= 47)
+            return spetials++;
+        }
+        return spetials >=3 ;
+    }
+
+void verifyPassword()
+{
+    string password;
+    cout << "Ingrese una contraseña: ";
+    cin >> password; 
+}
+
+int main()
+{
+    verifyPassword();
+    return 0;
+}
+
